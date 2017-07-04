f117320abc171e79daa4a60214a90f5a6d18da0f
Jack Turner <jack@turner.io>
2017-07-04 18:11:54 -0400

    first commit

---
+++ js/love-nikki.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  var columns = [
+    {"data": "Item Name", "title": "Name"},
+    {"data": "Rarity", "title": "Rarity"},
+    {"data": "Gorgeous", "title": "Gorgeous"},
+    {"data": "Elegant", "title": "Elegant"},
+    {"data": "Simple", "title": "Simple"},
+    {"data": "Lively", "title": "Lively"},
+    {"data": "Mature", "title": "Mature"},
+    {"data": "Cute", "title": "Cute"},
+    {"data": "Sexy", "title": "Sexy"},
+    {"data": "Pure", "title": "Pure"},
+    {"data": "Cool", "title": "Cool"},
+    {"data": "Warm", "title": "Warm"},
+    {"data": "Tags", "title": "Tags"},
+    {"data": "Item Type", "title": "Type"}
+  ]
+
+  var setup = function() {
+    Tabletop.init( { key: '1AjZyyqItfPIpzFLj7E76Rl2EqFNBQqPnqZCCAKJVFA4',
+                     callback: create_table,
+                     simpleSheet: true } )
+  }
+
+  var create_table = function(data, tabletop) {
+    console.log('Successfully processed!')
+    console.log(data)
+
+    $('#the-table').DataTable( {
+      'data': data,
+      'columns': columns,
+      'pageLength': 25
+    } )
+
+  }
+
+  setup()
+  
+})
